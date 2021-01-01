492afaf1d21b9e7f86b55343fc40352aefa7afa5
Peter Taylor <info@emersont1.co.uk>
2021-01-01 18:04:20 +0000

    added not stay logged in functionality

--- libbf/src/login.cpp
+++ libbf/src/login.cpp
@@ -41,7 +41,7 @@ libbf::login_cookie libbf::login_cookie::login(std::string email,
                              {"data%5Bpost_action%5D", "login"},
                              {"data%5BCustomer%5D%5Bemail_address%5D", email},
                              {"data%5BCustomer%5D%5Bpassword%5D", password},
-                             {"data%5Bremember_me%5D", "1"}});
+                             {"data%5Bremember_me%5D", remember_me?"1":"0"}});
 
   if (r.text.find("You have successfully logged in.") == std::string::npos)
     throw libbf::login_failed_exception();
