fbbf26bfe68bef317534b7a92df9f2d0065a71ff
Peter Taylor <info@emersont1.co.uk>
2021-01-02 08:25:05 +0000

    Beautified

--- libbf/include/libbf/downloads.hpp
+++ libbf/include/libbf/downloads.hpp
@@ -9,18 +9,19 @@
 namespace libbf {
 class download {
 public:
-  std::string      name;
-  std::uint32_t    download_number;
-  bool             m4b_available;
-  std::vector<std::pair<std::string,int>> supplementary_media;
+  std::string                              name;
+  std::uint32_t                            download_number;
+  bool                                     m4b_available;
+  std::vector<std::pair<std::string, int>> supplementary_media;
 
   download(std::string name, std::uint32_t download_number, bool m4b_available,
-           std::vector<std::pair<std::string,int>> supplementary_media)
+           std::vector<std::pair<std::string, int>> supplementary_media)
       : name(name), download_number(download_number),
         m4b_available(m4b_available), supplementary_media(supplementary_media) {
   }
 
   void download_mp3(
+      libbf::login_cookie & cookie,
       std::filesystem::path output_directory = std::filesystem::path());
 
   static std::vector<download> get_downloads(login_cookie & cookie);
