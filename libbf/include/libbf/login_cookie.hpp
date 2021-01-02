fd498761c215187b3569d60c77dfa25d2962cbf7
Peter Taylor <info@emersont1.co.uk>
2021-01-02 12:34:25 +0000

    added json I/O

--- libbf/include/libbf/login_cookie.hpp
+++ libbf/include/libbf/login_cookie.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include <nlohmann/json.hpp>
+
 namespace libbf {
 class login_cookie {
   std::string email;
@@ -19,6 +21,8 @@ public:
   static login_cookie   login(std::string email, std::string password,
                               bool remember_me = true);
   friend std::ostream & operator<<(std::ostream & os, const login_cookie & l);
+  friend void           to_json(nlohmann::json & j, const login_cookie & r);
+  friend void           from_json(const nlohmann::json & j, login_cookie & r);
   std::string           get_cakephp() { return cakephp_value; }
   std::string           get_customer() { return customer_value; }
   std::string           get_email() { return email; }
@@ -31,6 +35,20 @@ inline std::ostream & operator<<(std::ostream & os, const login_cookie & l) {
   os << "{Email: \"" << l.email << "\", CakePHP: \"" << l.cakephp_value
      << "\"; Customer: \"" << l.customer_value << "\"}";
   return os;
+}
+
+inline void to_json(nlohmann::json & j, const login_cookie & r) {
+  j = nlohmann::json{
+      {"email", r.email},
+      {"cakephp", r.cakephp_value},
+      {"customer", r.customer_value},
+  };
+}
+
+inline void from_json(const nlohmann::json & j, login_cookie & p) {
+  j.at("email").get_to(p.email);
+  j.at("cakephp").get_to(p.cakephp_value);
+  j.at("customer").get_to(p.customer_value);
 }
 
 } // namespace libbf
