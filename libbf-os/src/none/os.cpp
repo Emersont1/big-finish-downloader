912067d2727da5ac29fc264a678f19369ba0dd13
Peter Taylor <info@emersont1.co.uk>
2021-01-10 12:14:29 +0000

    GSettings works on windows it transpires

--- libbf-os/src/none/os.cpp
+++ libbf-os/src/none/os.cpp
@@ -5,31 +5,6 @@
 
 libbf::os::settings::settings() {}
 
-bool libbf::os::settings::get_prefer_m4b() {
-    return true;
-}
-
-void libbf::os::settings::set_prefer_m4b(bool x) {}
-
-bool libbf::os::settings::get_fallback_mp3() {
-    return true;
-}
-
-void libbf::os::settings::set_fallback_mp3(bool x) {}
-
-bool libbf::os::settings::get_download_extras() {
-    return true;
-}
-
-void libbf::os::settings::set_download_extras(bool x) {}
-
-std::filesystem::path libbf::os::settings::get_path() {
-    return std::filesystem::current_path();
-}
-
-void libbf::os::settings::set_path(std::filesystem::path x) {}
-
-
 void libbf::os::store(libbf::login_cookie l) {
     throw libbf::os::secret_write_failed_exception();
 }
