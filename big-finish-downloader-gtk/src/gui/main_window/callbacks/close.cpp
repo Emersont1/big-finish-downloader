d637c7fba73e93edf6cc12087e399d71533e41e6
Peter Taylor <info@emersont1.co.uk>
2021-01-21 21:49:35 +0000

    made it dispose before it called the quit function
    should fix issues with a non clean exit

--- big-finish-downloader-gtk/src/gui/main_window/callbacks/close.cpp
+++ big-finish-downloader-gtk/src/gui/main_window/callbacks/close.cpp
@@ -1,5 +1,13 @@
+#include <fstream>
 #include <libbf/gui/modules/main_window.hpp>
 
 void libbf::gui::main_window::close_cb(GtkWidget* wid, void* d) {
+    auto m = (libbf::gui::main_window*) d;
+    m->quit.set_value();
+    std::ofstream o(m->settings.get_path() / ".downloaded.json");
+    nlohmann::json j;
+    j["downloaded_ids"] = m->downloaded_ids;
+    j["shoud_download"] = m->shoud_download;
+    o << j;
     gtk_main_quit();
 }
