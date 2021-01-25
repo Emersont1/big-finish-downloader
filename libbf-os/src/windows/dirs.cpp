b6e5364b2678ea5fdb6f9689f90865f8b961270f
Peter Taylor <info@emersont1.co.uk>
2021-01-25 16:05:08 +0000

    fixed to use xdg-directories (and equivalent on windows)

--- libbf-os/src/windows/dirs.cpp
+++ libbf-os/src/windows/dirs.cpp
@@ -9,6 +9,10 @@ std::filesystem::path libbf::os::get_home() {
     return std::filesystem::path(std::getenv("USERPROFILE"));
 }
 
+std::filesystem::path libbf::os::get_music() {
+    return std::filesystem::path(std::getenv("CSIDL_MYMUSIC"));
+}
+
 std::string libbf::os::file_prefix() {
     return "explorer.exe ";
 }
