69bef75dd3afbadba5c950a5d02a2d7f2ccfacff
Peter Taylor <me@et1.uk>
2021-01-25 17:36:42 +0000

    fixed to use xdg-directories (and equivalent on windows) (#34)
    * fixed to use xdg-directories (and equivalent on windows)
    
    * moved to SHGetSpecialFolderPath
    
    * beautified

--- libbf-os/include/libbf/os/dirs.hpp
+++ libbf-os/include/libbf/os/dirs.hpp
@@ -5,5 +5,6 @@
 namespace libbf::os {
 std::filesystem::path get_cache();
 std::filesystem::path get_home();
+std::filesystem::path get_music();
 std::string file_prefix();
 } // namespace libbf::os
--- libbf-os/src/linux/dirs.cpp
+++ libbf-os/src/linux/dirs.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <libbf/os/dirs.hpp>
+#include <subprocess.hpp>
 
 std::filesystem::path libbf::os::get_cache() {
     return std::filesystem::path(std::getenv("HOME")) / ".cache" / "big-finish";
@@ -7,6 +8,19 @@ std::filesystem::path libbf::os::get_cache() {
 
 std::filesystem::path libbf::os::get_home() {
     return std::filesystem::path(std::getenv("HOME"));
+}
+
+std::filesystem::path libbf::os::get_music() {
+    subprocess::popen cmd("xdg-user-dir", {"MUSIC"});
+
+    int ret = cmd.wait();
+    if (ret == 0) {
+        std::string path;
+        cmd.stdout() >> path;
+        return std::filesystem::path(path);
+    } else {
+        return libbf::os::get_home() / "Music";
+    }
 }
 
 std::string libbf::os::file_prefix() {
