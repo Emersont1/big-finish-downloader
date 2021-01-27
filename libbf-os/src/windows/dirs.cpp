ba5606943de8f99ca613de4e3d6d0c2ac40be4b8
Peter Taylor <info@emersont1.co.uk>
2021-01-27 14:21:07 +0000

    Windows Issue with these directories, these fix it

--- libbf-os/src/windows/dirs.cpp
+++ libbf-os/src/windows/dirs.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <libbf/os/dirs.hpp>
 
-#include <ShlObj_core.h>
-
 std::filesystem::path libbf::os::get_cache() {
     return std::filesystem::path(std::getenv("LOCALAPPDATA")) / "big-finish";
 }
@@ -12,11 +10,6 @@ std::filesystem::path libbf::os::get_home() {
 }
 
 std::filesystem::path libbf::os::get_music() {
-    std::string music_path("\0", _MAX_PATH);
-
-    if (SHGetSpecialFolderPath(nullptr, music_path.data(), CSIDL_MYMUSIC, true)) {
-        return std::filesystem::path(music_path);
-    }
     return libbf::os::get_home() / "Music";
 }
 
